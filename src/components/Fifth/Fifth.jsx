33020b4b3e06f876e2e4072ff9e95786bc1c4e8f
Brahmasura <akhilesh@mymarketingpass.com>
2024-05-28 03:44:48 +0530

    5-28-2024 the calendar home functionality begins

--- src/components/Fifth/Fifth.jsx
+++ src/components/Fifth/Fifth.jsx
@@ -2,6 +2,7 @@ import style from "./fifth.module.scss";
 import Event from "../UpcomingEvent/Event";
 import arrow from "../../assets/down-chevron.svg";
 import calendar from "../../assets/calendar.svg";
+import { useState } from "react";
 
 const eventArray = [
   {
@@ -61,15 +62,56 @@ const eventArray = [
 ];
 
 const Fifth = () => {
+  // local states begins
+  const [mainDivClicked, setMainDivClicked] = useState(false);
+
+  // local states ends
+
+  const handleMainDivClick = () => {
+    setMainDivClicked(!mainDivClicked);
+  };
+
   return (
     <>
       <div className={style.mainDiv}>
-        <p className={style.title}>SCHEDULE 1-ON-1 COACHING</p>
-        <div className={style.secondDiv}>
+        {mainDivClicked ? (
+          <>
+            {/* selection Div begins */}
+            <div className={style.selectionDiv}>
+              <p className={style.clickedTitle}>
+                SCHEDULE YOUR <br /> COACHING SESSION
+              </p>
+              <p className={style.description}>
+                SELECT WHAT YOU NEED ASSISTANCE FOR
+              </p>
+            </div>
+
+            {/* selection Div ends */}
+          </>
+        ) : (
+          <>
+            <p onClick={handleMainDivClick} className={style.title}>
+              SCHEDULE 1-ON-1 COACHING
+            </p>
+          </>
+        )}
+
+        {/* second div begins */}
+        <div
+          className={`${style.secondDiv} ${
+            mainDivClicked && style.secondDivOff
+          }`}
+        >
           <p className={style.paraOne}>MyMarketingCalendar</p>
           <p className={style.paraTwo}>Upcoming community events</p>
 
-          <div className={style.eventsContainer}>
+          <div
+            className={
+              mainDivClicked
+                ? style.eventsContainerHidden
+                : style.eventsContainer
+            }
+          >
             {eventArray.map((item, index) => (
               <Event key={index} date={item.date} />
             ))}
@@ -77,19 +119,20 @@ const Fifth = () => {
 
           {/* the arrow box */}
 
-          <div className={style.arrowBox}>
-            <img className={style.leftArrow} src={arrow} alt="the left"/>
-            <img className={style.rightArrow} src={arrow} alt="the left"/>
+          <div
+            className={mainDivClicked ? style.arrowBoxHidden : style.arrowBox}
+          >
+            <img className={style.leftArrow} src={arrow} alt="the left" />
+            <img className={style.rightArrow} src={arrow} alt="the left" />
           </div>
 
-
           {/* calendar Div  */}
-           <div className={style.calendarDiv}>
-            <img src={calendar} alt="the calendar"/>
+          <div className={style.calendarDiv}>
+            <img src={calendar} alt="the calendar" />
             <p>CALENDAR VIEW </p>
-           </div>
-    
+          </div>
         </div>
+        {/* second div ends */}
       </div>
     </>
   );
