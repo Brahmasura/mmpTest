5cd6e7236e462c17b012f5959731db495b681c4a
Brahmasura <akhilesh@mymarketingpass.com>
2024-05-29 03:04:15 +0530

    5-29-2024 the fifth div that is calendar home functionality done

--- src/components/UpcomingEvent/Event.jsx
+++ src/components/UpcomingEvent/Event.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import style from "./event.module.scss";
 
-const Event = ({date, title}) => {
+const Event = ({ date, title }) => {
+  const [eventHover, setEventHover] = useState(false);
+
+  const handleEventHover = () => {
+    setEventHover(!eventHover);
+  };
+
   return (
     <>
-        <div className={style.mainDiv}>
-            <p className={style.date}>{date}</p>
-            <p className={style.title}>MEMBER PARTY MEETING - 2PM EST</p>
-        </div>
+      <div
+        className={`${style.mainDiv} ${eventHover && style.mainDiv__backRed}`}
+        onMouseEnter={handleEventHover}
+        onMouseLeave={handleEventHover}
+      >
+        <p className={style.date}>{date}</p>
+        <p className={style.title}>MEMBER PARTY {eventHover && (<br/>)} MEETING - 2PM EST</p>
+
+        {/* the RSVP div */}
+
+        {eventHover && (
+          <>
+            <div className={style.rsvpDiv}>
+              <p>RSVP</p>
+            </div>
+          </>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Event;
